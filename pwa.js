d4d35c182f8df2f9076a8292bf472051da402fdc
Keita, FURUSHIRO <furushiro.keita@rbizsol.com>
2018-06-07 10:02:56 +0900

    fe

--- pwa.js
+++ pwa.js
@@ -7,3 +7,28 @@ if ("serviceWorker" in navigator) {
         console.log("Registerd Service worker");
     });
 }
+
+function onClickRequest () {
+    request("./", {data: "data"}, 
+        (data) => {
+            console.dir(data);
+        },
+        (xhr) => {
+            console.dir(xhr);
+        });
+}
+
+function request (url, data, success, error) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                success(xhr.responseText);
+                return;
+            }
+            error(xhr);
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send(data);
+}
