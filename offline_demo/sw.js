de5ea943d08e2e4d636b11df849c34a2e5a19d10
Keita, FURUSHIRO <furushiro.keita@rbizsol.com>
2018-06-07 12:43:19 +0900

    k

--- offline_demo/sw.js
+++ offline_demo/sw.js
@@ -38,12 +38,57 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
     // ネットワークリクエストの傍受
-    console.log(event.request.url);
-    if (event.request.url.indexOf("static_datas") > -1) {
-        console.log("Response from cache!");
-        let res = caches.match(event.request, {
-            cacheName: CACHE_NAME
-        });
-        event.respondWith(res);
-    }
+    event.respondWith(
+        fetch(evnet.request).then((response) => {
+            if (!response || response.status !== 200 || response.type !== "basic") {
+                // 上手くいかなかったらキャッシュから
+                caches.match(event.request, {cacheName: CACHE_NAME}, (cache) => {
+                    if (cache) {
+                        console.log("response from ws");
+                        return cache;
+                    }
+                });
+            }
+            if (response.status === 200) {
+                // 通信が成立したらそのままリターン
+                let responseToCache = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseToCache);
+                });
+                console.log("response from server");
+                return response;
+            }
+        })
+    );
+    
+    
+    // console.log(event.request.url);
+    // if (event.request.url.indexOf("static_datas") > -1) {
+    //     console.log("Response from cache!");
+    //     let res = caches.match(event.request, {
+    //         cacheName: CACHE_NAME
+    //     });
+    //     event.respondWith(res);
+    // // }
+    // event.respondWith(
+    //     fetchRequest
+    //     caches.match(event.request, {cacheName: CACHE_NAME}).then((cache) => {
+    //         if (cache) {
+    //             // キャッシュがあればreturn
+    //             return cache;
+    //         }
+    //         let fetchRequest = evnet.request.clone();
+    //         return fetch(fetchRequest).then((response) => {
+    //             if (!response || response.status !== 200 || response.type !== "basic") {
+    //                 // レスポンスがイカれてたならそのままreturn
+    //                 return response;
+    //             }
+    //             let responseToCache = response.clone();
+    //             caches.open(CACHE_NAME).then((cache) => {
+    //                 cache.put(event.request, responseToCache);
+    //             });
+    //             return response;
+    //         });
+    //     })
+    // );
 });
