5e32aad5249a50a043a6e75d604cf8386c540a3e
Keita, FURUSHIRO <furushiro.keita@rbizsol.com>
2018-06-07 15:35:05 +0900

    r

---
+++ notification_demo/assets/js/sample.js
@@ -0,0 +1,33 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/notification_demo/sw.js").then(() => {
+        console.log("Registerd Service worker");
+    });
+}
+
+Notification.requestPermission().then(function(permissions) {
+    switch (permissions) {
+        case "granted":
+            // 許可された場合
+            break;
+        case "denied":
+            // ブロックされた場合
+            break;
+        case "default":
+            // 無視された場合
+            break;
+        default:
+            break;
+    }
+});
+
+
+function onClickNotification(){
+
+    const notification = new Notification("Thanks!", {
+        body: "Notification!",
+        icon: "./assets/icon-192.png",
+        tag: "notification"
+    });
+
+    return false;
+}
