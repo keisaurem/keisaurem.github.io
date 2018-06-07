809e59576696245b1d148669899ec233d57f336e
Keita, FURUSHIRO <furushiro.keita@rbizsol.com>
2018-06-07 10:09:28 +0900

    fix

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener("fetch", (event) => {
     // ネットワークリクエストの傍受
     console.dir(event.request);
-    if (evvent.request.url.indexOf("takashi") > -1) {
+    if (event.request.url.indexOf("takashi") > -1) {
         event.respondWith(new Response("koyubi"));
     }
 });
