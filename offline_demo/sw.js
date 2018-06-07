c7aa57304eb2fee18bffe3b59483573cf55ac26b
Keita, FURUSHIRO <furushiro.keita@rbizsol.com>
2018-06-07 10:53:33 +0900

    fix

--- offline_demo/sw.js
+++ offline_demo/sw.js
@@ -21,7 +21,7 @@ self.addEventListener("install", (event) => {
     );
 });
 
-self.addEventListener("activate", (evnet) => {
+self.addEventListener("activate", (event) => {
     event.waitUntil(
         caches.keys().then((keys) => {
             let promises = [];
