b9e17bd66459d02589a56a89a8c1a7b563fabad4
Keita, FURUSHIRO <furushiro.keita@rbizsol.com>
2018-06-07 09:49:52 +0900

    vv

--- pwa.js
+++ pwa.js
@@ -1,59 +1,9 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./service-worker.js").then(function() {
+    let options = {
+        scope: "./"
+    };
+
+    navigator.serviceWorker.register("./service-worker.js", options).then(() => {
         console.log("Registerd Service worker");
     });
 }
-
-// Notification.requestPermission().then(function(permissions) {
-//     if (permissions === "granted") {
-//         // show_sample_notification();
-//     }
-//     switch (permissions) {
-//         case "granted":
-//             // 許可された場合
-//             // show_sample_notification();
-//             break;
-//         case "denied":
-//             // ブロックされた場合
-//             break;
-//         case "default":
-//             // 無視された場合
-//             break;
-//         default:
-//             break;
-//     }
-// });
-
-// navigator.serviceWorker.ready.then(function(registration) {
-//     return registration.pushManager.subscribe({
-//         userVisibleOnly: true,
-//         applicationServerKey: serverPublicKey
-//     }).then(function (subscription) {
-//         const endpoint = subscription.endpoint;
-//         const publickey = encodeBase64URL(subscription.getKey("p256dh"));
-//         const authSecret = encodeBase64URL(subscription.getKey("auth"));
-//         let contentEncoding;
-//         if ("supportedContentEncodings" in pushManager) {
-//             contentEncoding = pushManager.supportedContentEncodings.includes("ase128gcm") ? "aes128gcm" : "aesgcm";
-//         } else {
-//             contentEncoding = "aesgcm";
-//         }
-//     });
-// });
-
-// function show_sample_notification() {
-//     const notification = new Notification("Thanks!", {
-//         body: "Thenk you for apply of notification!",
-//         icon: "./icon-192.png",
-//         data: {
-//             hoge: "hoge"
-//         }
-//     });
-// }
-
-// function encodeBase64URL(buffer) {
-//     return btoa(String.fromCharCode.apply(null, new Uint8Array(buffer)))
-//         .replace(/\+/g, "-")
-//         .replace(/\//g, "_")
-//         .replace(/=+$/, "");
-// }
--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,4 @@
-// service-worker.js
-self.addEventListener("install", function(e) {
-    console.log("[ServiceWorker] Install");
-    // e.waitUntil(
-    //     caches.open("airhorner").then((cache) => {
-    //         return cache.addAll([
-    //             "/",
-    //             "/index.html",
-    //             "/index.html?utm_source=homescreen",
-    //             "/?utm_source=homescreen",
-    //             "/icon-192.png",
-    //             "/icon-256.png",
-    //             "/pwa.js"
-    //         ]);
-    //     })
-    // );
+self.addEventListener("fetch", function(event) {
+    // ネットワークリクエストの傍受
+    console.dir(event.request);
 });
-
-self.addEventListener("activate", function(e) {
-    console.log("[ServiceWorker] Activate");
-});
-
-// push受信
-// self.addEventListener("push", function(e) {
-//     let data = e.data.json();
-//     return e.waitUntil(
-//         self.registration.showNotification (
-//             data.title,
-//             {
-//                 icon: "./icon-192.png",
-//                 body: data.body,
-//                 tag: data.tag
-//             }
-//         )
-//     );
-// });
-
-// 現状では、この処理を書かないとService Workerが有効と判定されないようです
-self.addEventListener("fetch", function(event) {});
