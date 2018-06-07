f2766c306669633c1ddf2550e4f74ff38c13a290
Keita, FURUSHIRO <furushiro.keita@rbizsol.com>
2018-06-07 10:05:21 +0900

    ge

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", (event) => {
     // ネットワークリクエストの傍受
     console.dir(event.request);
+    console.dir(event.request.url);
+    event.respondWith(new Response("hogehoge"));
 });
